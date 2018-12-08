4c7402b51ad49902e887572da5a965d93dd6b761
Gjulio <gjuliojakova@yahoo.com>
2018-12-08 16:37:13 +0100

    Added case class to verify equality and toString

--- src/main/scala/lab04/Complex.scala
+++ src/main/scala/lab04/Complex.scala
@@ -1,5 +1,7 @@
 package lab04
 
+import lab04.Complex.ComplexImplCase
+
 trait Complex {
   def re: Double
   def im: Double
@@ -8,20 +10,24 @@ trait Complex {
 }
 
 object Complex {
-  def apply(re:Double, im:Double): Complex = new ComplexImpl(re, im)
+  def apply(re:Double, im:Double): Complex =  new ComplexImpl(re, im)
 
   private class ComplexImpl( var re: Double, var im: Double) extends Complex{
 
-    override def +(c: Complex): Complex = {
+    override def +(c: Complex): Complex =
       Complex(this.re+c.re, this.im+c.im)
-    }
 
-    override def *(c: Complex): Complex = {
+
+    override def *(c: Complex): Complex =
       Complex(this.re*c.re - this.im*c.im, this.re*c.im + this.im*c.re)
-    }
 
     override def toString = "ComplexImpl(" + re + ", " + im + ")"
 
+  }
+
+  case class ComplexImplCase(re: Double, im: Double) extends Complex {
+    override def +(c: Complex): Complex = Complex(this.re+c.re, this.im+c.im)
+    override def *(c: Complex): Complex = Complex(this.re*c.re - this.im*c.im, this.re*c.im + this.im*c.re)
   }
 }
 
@@ -31,6 +37,10 @@ object TryComplex extends App {
   println(c, c.re, c.im) // (ComplexImpl(18.0,21.0),18.0,21.0)
   val c2 = a(0)*a(1)
   println(c2, c2.re, c2.im) // (ComplexImpl(-10.0,30.0),-10.0,30.0)
+
+  println(Complex(10,20) == Complex(10,20)) // false
+  println(ComplexImplCase(10,20) == ComplexImplCase(10,20)) //true
+  println(ComplexImplCase(10,20)) // ComplexImplCase(10.0,20.0)
 }
 
 /** Hints:
