05c00c2f8019b331958d081566e382f720e6f6f4
Gjulio <gjuliojakova@yahoo.com>
2018-12-12 11:42:02 +0100

    Implemented mixin NotTwoConsecutive & tested two mixins together

--- src/main/scala/lab05/Parser.scala
+++ src/main/scala/lab05/Parser.scala
@@ -12,7 +12,7 @@ package lab05
 abstract class Parser[T] {
   def parse(t: T): Boolean  // is the token accepted?
   def end(): Boolean        // is it ok to end here
-  def parseAll(seq: Seq[T]): Boolean = (seq forall {parse(_)}) & end() // note &, not &&
+  def parseAll(seq: Seq[T]): Boolean = (seq forall parse) & end() // note &, not &&
 }
 
 case class BasicParser(chars: Set[Char]) extends Parser[Char] {
@@ -22,36 +22,54 @@ case class BasicParser(chars: Set[Char]) extends Parser[Char] {
 
 trait NonEmpty[T] extends Parser[T]{
   private[this] var empty = true
-  abstract override def parse(t: T) = {empty = false; super.parse(t)} // who is super??
-  abstract override def end() = !empty && {empty = true; super.end()}
+  abstract override def parse(t: T): Boolean = {empty = false; super.parse(t)} // who is super??
+  abstract override def end(): Boolean = !empty && {empty = true; super.end()}
 }
 
 class NonEmptyParser(chars: Set[Char]) extends BasicParser(chars) with NonEmpty[Char]
 
 trait NotTwoConsecutive[T] extends Parser[T]{
-  // ???
-}
+  var previousChar: Char =  _
 
-class NotTwoConsecutiveParser(chars: Set[Char]) extends BasicParser(chars) // ??? with ...
+  abstract override def end(): Boolean = {
+    previousChar = ' '
+    true
+  }
+
+  abstract override def parse(c: T): Boolean = {
+    if(!previousChar.equals(c)){
+      previousChar = c.toString.charAt(0)
+      super.parse(c)
+    }
+    else {
+      false
+    }
+  }
+
+ }
+
+class NotTwoConsecutiveParser(chars: Set[Char]) extends BasicParser(chars) with NotTwoConsecutive[Char]
 
 
 object TryParsers extends App {
-  val parser = new BasicParser(Set('a','b','c'))
+  val parser = BasicParser(Set('a','b','c'))
   println(parser.parseAll("aabc".toList)) // true
   println(parser.parseAll("aabcdc".toList)) // false
   println(parser.parseAll("".toList)) // true
-
+  println()
   // Note NonEmpty being "stacked" on to a concrete class
   // Bottom-up decorations: NonEmptyParser -> NonEmpty -> BasicParser -> Parser
   val parserNE = new NonEmptyParser(Set('0','1'))
   println(parserNE.parseAll("0101".toList)) // true
   println(parserNE.parseAll("0123".toList)) // false
   println(parserNE.parseAll(List())) // false
+  println()
 
   val parserNTC = new NotTwoConsecutiveParser(Set('X','Y','Z'))
   println(parserNTC.parseAll("XYZ".toList)) // true
   println(parserNTC.parseAll("XYYZ".toList)) // false
   println(parserNTC.parseAll("".toList)) // true
+  println()
 
   // note we do not need a class name here, we use the structural type
   val parserNTCNE = new BasicParser(Set('X','Y','Z')) with NotTwoConsecutive[Char] with NonEmpty[Char]
